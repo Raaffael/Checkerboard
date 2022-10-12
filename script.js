04bf2d1b3fc3c60ce657d0be5b1585c88dbecb69
Jonathan Robles <roblesjonathan96@gmail.com>
2022-10-12 14:43:32 -0600

    hi

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Your JS goes here
+let newDiv = document.createElement('div');
+newDiv.style.width = '11.1%';
+newDiv.style.float = 'left';
+newDiv.style.paddingBottom = '11.1%';
+
+var body = document.createElement('body');
+body.style.margin = '0px';
+
+for(var i=0; i<81;i++){
+    
+}
